5a9a57363c6da666e8d27ecf3e884538cb3f1c38
reficul31 <reficul31@gmail.com>
2017-02-19 09:29:17 +0530

    Changing the models

--- app/models.py
+++ app/models.py
@@ -1,9 +1,32 @@
 from flask import Flask
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.types import VARCHAR, CHAR, JSON
+from sqlalchemy.types import VARCHAR, CHAR, JSON, Integer
 from app import db
 from random import choice
+import inspect
+# A decorator to automagically assign init variables.
+def instanceVariables(func):
+	def returnFunc(*args, **kwargs):
+		self_var = args[0]
+
+		arg_spec = inspect.getargspec(func)
+		argument_names = arg_spec[0][1:]
+		defaults = arg_spec[3]
+		if defaults is not None:
+			default_argdict = dict(zip(reversed(argument_names), reversed(defaults)))
+			self_var.__dict__.update(default_argdict)
+
+		arg_dict = dict(zip(argument_names, args[1:]))
+		self_var.__dict__.update(arg_dict)
+
+		valid_keywords = set(kwargs)&set(argument_names)
+		kwarg_dict = {k: kwargs[k] for k in valid_keywords}
+		self_var.__dict__.update(kwarg_dict)
+
+		func(*args, **kwargs)
+
+	return returnFunc
 
 class CRUD():
 	def add(self, resource):
@@ -27,7 +50,7 @@ class User(Base):
 	id = Column(CHAR(5), unique=True, primary_key=True)
 	username = Column(VARCHAR(30), default=None, nullable=False, unique = True)
 	password = Column(VARCHAR(16), default=None, nullable= False)
-	forms = relationship('Child')
+	# forms = relationship('Child')
 
 	def __init__(self, username, password):
 		self.id = ''.join(choice('0123456789') for i in range(5)) 
@@ -38,16 +61,14 @@ class User(Base):
 		return "<User(username:%s)>"%self.username
 
 class Forms(Base):
-	id = Column(CHAR(5), default=None, primary_key=True)
+	id = Column(Integer, primary_key=True, autoincrement=True)
 	name = Column(VARCHAR(16), default = None )
+	password = Column(VARCHAR(20), default = None)
 	json = Column(VARCHAR(1000), default = None, nullable=False )
-	user_id = Column(CHAR(5), ForeignKey('user.id'))
-
-	def __init(self, name, json, user_id):
-		self.id = ''.join(choice('0123456789') for i in range(5)) 
-		self.name = name
-		self.json = json
-		user_id = user_id
+	# user_id = Column(CHAR(5), ForeignKey('user.id'))
+	@instanceVariables
+	def __init__(self, name, password, json):
+		pass
 
 	def __repr__(self):
 		return "<Form(name:%s)>"%self.name
