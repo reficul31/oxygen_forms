a1646e942c6e326880ec5765a3621124f70b9c78
reficul31 <reficul31@gmail.com>
2017-02-24 23:37:43 +0530

    Adding basic form rendering and creating

--- app/__init__.py
+++ app/__init__.py
@@ -1,62 +1,67 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api, Resource, reqparse
-from flask import Flask, render_template, jsonify
+from sqlalchemy.sql.expression import select
+from flask import Flask, render_template, jsonify, request
 from flask_restful import Api, Resource, reqparse
 import json
-# Initializing the app with the flask microframework
 app = Flask(__name__, instance_relative_config=True)
-# Setting configuration for the app
 app.config.from_pyfile("config.py")
-# Initializing the restful api
-api = Api(app)
-# Initializing the sqlalchemy
 db = SQLAlchemy(app)
-# Avoiding the circular imports
 from models import User, Forms
-# Default route
-@app.route('/')
+
+@app.route('/', methods=['GET','POST'])
 def index():
 	return render_template('index.html')
-# First end point
-class Form(Resource):
-	# Post request come here
-	def post(self):
-		response = ''
-		i=0
-		form = None
+
+@app.route('/design',methods=['GET','POST'])
+def designform():
+	res = ''
+	i=0
+	form = None
+	if request.method == 'POST':
+		data_file = request.form['sender']
+		data = json.loads(data_file)
 		try:
-			# Request parser used to parse the post requests
-			parser = reqparse.RequestParser()
-			parser.add_argument('formstring',type=str, help='Form JSON String')
-			args = parser.parse_args()
-			formstring = args['formstring']
-			# Loading the json from the string
-			formdict = json.loads(formstring)
-			# Saving the form in the database
-			form = Forms(name=formdict['formname'], password=formdict['formpass'], 
-				json=str(formdict['form_data']))
+			form = Forms(name=data['formname'], password=data['formpass'], json=json.dumps(data['form_data']))
 			form.add(form)
-			# Making the form table
-			query = "CREATE TABLE "+" "+formdict["formname"]+"("
-			for k in formdict['form_data']['fields']:
+			query = "CREATE TABLE "+" "+data["formname"]+"("
+			for k in data['form_data']['fields']:
 				if i != 0:
 					query = query+","
 				query=query+str(k['label'].replace(" ","_").replace("?","").lower())+" VARCHAR(1000)"
 				i+=1
 			query = query+")"
-			# Execute the query
 			db.engine.execute(query)
-			# Commiting the database
 			db.session.commit()
-			# Sending success response
-			response = {'status_code':110}
-		except Exception as e:
-			# Catching exceptions
+			res = 'Everything is Awesome'
+		except Exception:
 			if form is not None:
-				# Rollback if an error occurred
 				form.rollback()
-			# Response of failure sent
-			response = {'status_code':111}
-		return jsonify(response)
-# Adding the resource to the api end point
-api.add_resource(Form, '/form')
+				res = 'WRONG!!!'
+	return render_template('makeform.html', res = res)
+
+@app.route('/fill/<id>', methods=['GET','POST'])
+def fillform(id):
+	form = Forms.query.filter_by(id = id).with_entities(Forms.name, Forms.json, Forms.id).first()
+	res = ''
+	if request.method == 'POST':
+		try:
+			query = "INSERT INTO "+form.name+"("
+			for field in request.form:
+				if field !='Submit':
+					query = query+str(field)+","
+			query = query[:-1]
+			query = query+")"
+			query = query+" VALUES("
+			for field in request.form:
+				if field !='Submit':
+					query = query+'"'+str(request.form[field])+'"'+","
+			query = query[:-1]
+			query = query+")"
+			print(query)
+			db.session.execute(query)
+			db.session.commit()
+			res = 'Form Submitted Successfully'
+		except Exception:
+			res = 'An Error occured'
+	rendform = {'name':form.name,'json':json.loads(form.json),'id':form.id}
+	return render_template('fillform.html', form = rendform, res=res)
