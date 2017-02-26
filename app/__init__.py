7e9aaa344b074309c3d51d3d46d8ddeb6e367f64
reficul31 <reficul31@gmail.com>
2017-02-26 10:48:03 +0530

    Adding the registration and login backend

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,117 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql.expression import select
-from flask import Flask, render_template, jsonify, request
-from flask_restful import Api, Resource, reqparse
+from flask_login import LoginManager, login_required, login_user, current_user, logout_user, UserMixin
+from datetime import timedelta
+from flask import Flask, render_template, jsonify, request, redirect
 import json
+from itsdangerous import URLSafeTimedSerializer
+
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile("config.py")
+app.config["REMEMBER_COOKIE_DURATION"] = timedelta(days=1)
+
 db = SQLAlchemy(app)
 from models import User, Forms
+login_serializer = URLSafeTimedSerializer(app.secret_key)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "/login"
+login_manager.session_protection = "strong"
+
+def checkUserAuth(func):
+	@wraps(func)
+	def returnFunc(*args, **kwargs):
+		if not current_user.is_authenticated:
+			abort(405)
+
+		return func(*args, **kwargs)
+
+	return returnFunc
+
+class FormUser(UserMixin):
+	def __init__(self, id, username, password):
+		self.id = id
+		self.username = username
+		self.password = password
+
+	def get_auth_token(self):
+		data = [str(self.id), self.password]
+		return login_serializer.dumps(data)
+
+	@staticmethod
+	def get(id=None, username=None):
+		user = None
+		if id:
+			user = User.query.filter_by(id = id).first()
+		elif username:
+			user = User.query.filter_by(username = username).first()
+
+		if user:
+			return FormUser(user.id, user.username, user.password)
+		return None
+
+@login_manager.user_loader
+def load_user(id):
+	return FormUser.get(id)
+
+@login_manager.token_loader
+def load_token(token):
+	max_age = app.config["REMEMBER_COOKIE_DURATION"].total_seconds()
+
+	# Decrypt the Security Token, data = [username, hashpass]
+	data = login_serializer.loads(token, max_age=max_age)
+
+	user = FormUser.get(data[0])
+
+	if user and data[1] == user.password:
+		return user
+
+	return None	
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+	res =''
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		if not current_user.is_authenticated:
+			user = FormUser.get(username= username)
+			if user and password == user.password:
+				login_user(user, remember=True)
+			else:
+				res = 'Enter valid login credentials'
+	if current_user.is_authenticated:
+		return redirect('/')
+	else:
+		return render_template('login.html', res = res)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+	res = ''
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		try:
+			user = User(username = username, password=password)
+			user.add(user)
+			res= 'Registration Successfull'
+		except Exception:
+			res = 'Registration Unsuccessfull'
+	return render_template('register.html', res=res)
 
 @app.route('/', methods=['GET','POST'])
+@login_required
 def index():
+	print(current_user.id)
 	return render_template('index.html')
 
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+	logout_user()
+	return redirect('/login')
+
 @app.route('/design',methods=['GET','POST'])
+@login_required
 def designform():
 	res = ''
 	i=0
@@ -48,7 +147,7 @@ def fillform(id):
 			query = "INSERT INTO "+form.name+"("
 			for field in request.form:
 				if field !='Submit':
-					query = query+str(field)+","
+					query = query+str(field.replace(" ","_").replace("?","").lower())+","
 			query = query[:-1]
 			query = query+")"
 			query = query+" VALUES("
@@ -57,7 +156,6 @@ def fillform(id):
 					query = query+'"'+str(request.form[field])+'"'+","
 			query = query[:-1]
 			query = query+")"
-			print(query)
 			db.session.execute(query)
 			db.session.commit()
 			res = 'Form Submitted Successfully'
