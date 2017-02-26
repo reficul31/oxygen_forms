9ccce663e258d8ce303ca89bd2cbad0e21e3170f
reficul31 <reficul31@gmail.com>
2017-02-26 10:48:30 +0530

    Changing the secret key

--- instance/config.py
+++ instance/config.py
@@ -2,5 +2,5 @@ import os
 basedir  = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir,'../app.sqlite')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, '../migrations')
-SECRET_KEY = '=54e!ka48(is5t)_@)!+$&-$j6jwkf-$a_$$zxn&fir*^3r5'
+SECRET_KEY = 'db51542b6345e5d681809ffe9b2ec271'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
